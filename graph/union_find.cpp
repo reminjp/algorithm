5dcf7da288c5f64faf5cfa9f776c02d9345170f5
remin <noreply@remin.jp>
2019-02-13 01:08:03 +0900

    update union find

--- graph/union_find.cpp
+++ graph/union_find.cpp
@@ -6,9 +6,10 @@ class union_find {
  private:
   vector<int> parent;
   vector<int> rank;
+  vector<int> sizes;
 
  public:
-  union_find(int n) : parent(n), rank(n) {
+  union_find(int n) : parent(n), rank(n), sizes(n, 1) {
     for (int i = 0; i < n; i++) parent[i] = i;
   }
   void unite(int a, int b) {
@@ -21,12 +22,15 @@ class union_find {
       parent[b] = a;
       if (rank[a] == rank[b]) rank[a]++;
     }
+    sizes[find(a)] = sizes[a] + sizes[b];
   }
   int find(int a) { return parent[a] == a ? a : parent[a] = find(parent[a]); }
+  int size(int a) { return sizes[find(a)]; }
 };
 
 int main() {
   union_find uf(6);
+  assert(uf.size(2) == 1);
   uf.unite(0, 2);
   uf.unite(1, 3);
   uf.unite(5, 1);
@@ -36,6 +40,7 @@ int main() {
   assert(uf.find(3) == 1);
   assert(uf.find(4) == 4);
   assert(uf.find(5) == 1);
+  assert(uf.size(2) == 2);
   uf.unite(0, 1);
   assert(uf.find(0) == 0);
   assert(uf.find(1) == 0);
@@ -43,4 +48,5 @@ int main() {
   assert(uf.find(3) == 0);
   assert(uf.find(4) == 4);
   assert(uf.find(5) == 0);
+  assert(uf.size(2) == 5);
 }
