4d935a1bd33d868a408a03df1a5a63e9112280be
remin <noreply@remin.jp>
2019-09-22 16:43:21 +0900

    refactor chinese_remainder_theorem

--- integer/chinese_remainder_theorem.cpp
+++ integer/chinese_remainder_theorem.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 
 // Garner algorithm
-template <typename T, T mod>
+template <typename T, T MOD>
 class ChineseRemainderTheorem {
  private:
+  using S = typename std::vector<T>::size_type;
   T ExtendedGcd(T a, T b, T &x, T &y) {
     if (b) {
       T d = ExtendedGcd(b, a % b, y, x);
@@ -24,12 +25,12 @@ class ChineseRemainderTheorem {
 
  public:
   T operator()(const std::vector<T> &a, std::vector<T> m) {
-    m.push_back(mod);
+    m.push_back(MOD);
     std::vector<T> coefficients(m.size(), 1);
     std::vector<T> constants(m.size(), 0);
-    for (int k = 0; k < a.size(); k++) {
+    for (S k = 0; k < a.size(); k++) {
       long long t = ((a[k] - constants[k]) * ModuloInverse(coefficients[k], m[k]) % m[k] + m[k]) % m[k];
-      for (int i = k + 1; i < m.size(); i++) {
+      for (S i = k + 1; i < m.size(); i++) {
         constants[i] += t * coefficients[i];
         constants[i] %= m[i];
         coefficients[i] *= m[k];
@@ -41,10 +42,8 @@ class ChineseRemainderTheorem {
 };
 
 // test
-constexpr int mod = 1e9 + 7;
-
 int main() {
-  ChineseRemainderTheorem<long long, mod> crt;
+  ChineseRemainderTheorem<long long, int(1e9 + 7)> crt;
   assert(crt({2, 3, 2}, {3, 5, 7}) == 23);
   assert(crt({0, 3, 4}, {3, 4, 5}) == 39);
 }
