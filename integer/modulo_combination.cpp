7cf098445da75e1c44ae571b5bf74ef3ccda6b27
remin <noreply@remin.jp>
2019-09-22 16:43:45 +0900

    refactor modulo_combination

--- integer/modulo_combination.cpp
+++ integer/modulo_combination.cpp
@@ -1,27 +1,27 @@
 #include <cassert>
 #include <vector>
 
-template <typename T, T mod>
+template <typename T, T MOD>
 class ModuloCombination {
  private:
   std::vector<T> factrial, inverse_factrial;
   T Pow(T x, T y) {
     if (y == 0) return 1;
-    if (y % 2 == 0) return Pow(x * x % mod, y / 2);
-    return x * Pow(x, y - 1) % mod;
+    if (y % 2 == 0) return Pow(x * x % MOD, y / 2);
+    return x * Pow(x, y - 1) % MOD;
   }
 
  public:
   ModuloCombination(T n) : factrial(n + 1), inverse_factrial(n + 1) {
     inverse_factrial[0] = factrial[0] = 1;
     for (T i = 1; i <= n; i++) {
-      factrial[i] = factrial[i - 1] * i % mod;
-      inverse_factrial[i] = Pow(factrial[i], mod - 2);
+      factrial[i] = factrial[i - 1] * i % MOD;
+      inverse_factrial[i] = Pow(factrial[i], MOD - 2);
     }
   }
   T operator()(T n, T k) {
     if (n < 0 || k < 0 || n < k) return 0;
-    return factrial[n] * inverse_factrial[k] % mod * inverse_factrial[n - k] % mod;
+    return factrial[n] * inverse_factrial[k] % MOD * inverse_factrial[n - k] % MOD;
   }
 };
 
