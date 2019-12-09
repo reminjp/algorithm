3453ae1a75535472d4921d6dd3d82336e648d3a7
remin <noreply@remin.jp>
2019-12-10 02:39:53 +0900

    refactor timer

--- utility/timer.cpp
+++ utility/timer.cpp
@@ -1,15 +1,14 @@
 #include <cassert>
 #include <chrono>
-using namespace std;
 
 class Timer {
  public:
   Timer() : time_begin(Now()) {}
-  int Elapsed() { return chrono::duration_cast<chrono::milliseconds>(Now() - time_begin).count(); }
+  int Elapsed() { return std::chrono::duration_cast<std::chrono::milliseconds>(Now() - time_begin).count(); }
 
  private:
-  chrono::system_clock::time_point time_begin;
-  static chrono::system_clock::time_point Now() { return chrono::system_clock::now(); }
+  std::chrono::system_clock::time_point time_begin;
+  static std::chrono::system_clock::time_point Now() { return std::chrono::system_clock::now(); }
 };
 
 // test
--- utility/timer_legacy.cpp
+++ utility/timer_legacy.cpp
@@ -1,7 +1,6 @@
 #include <time.h>
 #include <cassert>
 #include <iostream>
-using namespace std;
 
 class Timer {
  public:
